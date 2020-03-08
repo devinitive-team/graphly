5a9dab53f810192431a94c4bbbcb494bc3add7be
Łukasz Gut <blinku.dev@gmail.com>
2020-03-08 16:12:41 +0100

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="graphly-pkg-devinitive",
+    name="graphly-devinitive",
     version="1.0.2",
     author="devinitive",
     author_email="devinitive@placeholder.com",
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/devinitive-team/graphly",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "networkx",
+        "matplotlib"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
