226aee68131e90732293c50303918cdc5315336d
Łukasz Gut <blinku.dev@gmail.com>
2020-03-14 14:00:35 +0100

    Implemented degree sequence validation

---
+++ graphly/algorithm/algorithm.py
@@ -0,0 +1,18 @@
+def degree_seq(sequence):
+    seq = sequence.copy()
+
+    seq.sort(reverse=True)
+    while True:
+        if all(v == 0 for v in seq):
+            return True
+
+        if seq[0] < 0 or seq[0] >= len(seq):
+            return False
+
+        for i in range(1, seq[0] + 1):
+            seq[i] -= 1
+            if seq[i] < 0:
+                return False
+
+        seq[0] = 0
+        seq.sort(reverse=True)
--- graphly/graph.py
+++ graphly/graph.py
@@ -1,5 +1,6 @@
 from graphly.reader import reader
 from graphly.plotter import plotter
+from graphly.representation import representation
 
 
 class graph:
@@ -9,6 +10,23 @@ class graph:
     @classmethod
     def from_file(cls, filepath):
         return cls(reader.read(filepath))
+
+    @classmethod
+    def from_degree_seq(cls, sequence):
+        adj_list = [[] for _ in range(len(sequence))]
+        sequence.sort(reverse=True)
+        for i in range(len(sequence)):
+            counter = 0
+            j = i + 1
+            while counter < sequence[i]:
+                if sequence[j] > 0:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
+                    sequence[j] -= 1
+                    counter += 1
+                j += 1
+
+        return cls(representation.adjacency_list(adj_list))
 
     def print(self):
         self.graph_representation.print()
