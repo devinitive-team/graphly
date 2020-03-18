f8209d183627ffb7c2285dfd6f66ef8a2c4e2be5
kacperkapusciak <kapusciak.k@gmail.com>
2020-03-18 19:29:22 +0100

    Change largest_component to return values, add docs, add is_connected()

--- examples/largest_component_example.py
+++ examples/largest_component_example.py
@@ -1,5 +1,10 @@
+from pprint import pformat, pprint
 from graphly.algorithm import algorithm
 from graphly.graph import graph
 
 g = graph.from_file("disconnected_graph.json")
-algorithm.largest_component(g)
+components, largest_component = algorithm.largest_component(g)
+
+formatted = pformat(components, indent=2)
+pprint(formatted)
+print(f"Largest component: {largest_component}")
--- graphly/algorithm/algorithm.py
+++ graphly/algorithm/algorithm.py
@@ -3,6 +3,10 @@ import itertools
 
 
 def is_degree_seq(sequence):
+    """
+    :param sequence: sequence as an array of integers
+    :return: boolean
+    """
     seq = sequence.copy()
 
     seq.sort(reverse=True)
@@ -32,34 +36,47 @@ def components_helper(nr, v, graph, comp):
 
 
 def components(graph):
-    nr = 0
+    """
+    :param graph
+    :return: array of integers ordered that every vertex belongs to a component
+    """
+    nr = 0  # which connected component
     vert = graph.nodes()
-    comp = [-1 for v in vert]
+    comp = [-1 for _ in vert]  # no vertices were visited
     for v in vert:
         if comp[v] == -1:
             nr += 1
-            comp[v] = nr
+            comp[v] = nr  # mark v as visited and belongs to 'nr' component
             components_helper(nr, v, graph, comp)
     return comp
 
 
 def largest_component(graph):
+    """
+    :param graph
+    :return: a dictionary of vertices grouped in components,
+             a number which component is the largest
+    """
     component_array = components(graph)
     num_of_components = max(component_array)
+    components_dict = {}
 
     for i in range(1, num_of_components + 1):
-        print(f"{i}) ", end='')
+        components_dict[i] = []
         for index, value in enumerate(component_array):
             if i == value:
-                print(f"{index} ", end='')
-        print()
+                components_dict[i].append(index)
 
     counted_components = dict((comp, component_array.count(comp)) for comp in component_array)
     largest = max(counted_components, key=counted_components.get)
-    print(f"Largest component: {largest}")
+    return components_dict, largest
 
 
 def randomize_graph(graph, num):
+    """
+    :param graph
+    :param num: number of randomizations
+    """
     for _ in range(num):
         edges = graph.edges()
         random_edges = random.choices(edges, k=2)
@@ -91,5 +108,13 @@ def is_hamiltonian(graph):
 
         if valid:
             return True
+
+    return False
+
+
+def is_connected(graph):
+    components_dict = largest_component(graph)
+    if len(components_dict) == 1:
+        return True
 
     return False
