ab583a410c3cdebf87cfd12b065560308a3ad225
Łukasz Gut <blinku.dev@gmail.com>
2020-03-28 11:28:19 +0100

    gitflow-release-stash: 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="graphly",
-    version="1.0.4",
+    version="1.0.6",
     author="devinitive",
     author_email="devinitive@placeholder.com",
     description="A small library for studying graphs",
