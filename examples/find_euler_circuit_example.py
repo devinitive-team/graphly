6bc95ec707b15c54312b4b4c89d9e8e43be3d341
Łukasz Gut <blinku.dev@gmail.com>
2020-03-28 11:25:43 +0100

    Fix formatting.

--- examples/find_euler_circuit_example.py
+++ examples/find_euler_circuit_example.py
@@ -1,11 +1,7 @@
 from graphly.generator import generator
 from graphly.algorithm import algorithm
-from graphly.graph import graph
 
 g = generator.generate("eulerian", 8)
 
-# g = graph.from_file("eulerian_graph.json")
-
 eulerian_circuit = algorithm.find_eulerian_circuit(g)
 print(" -> ".join(map(str, eulerian_circuit)))
-
