9b9b2d787be3f4833f705084345e29acb2e94fde
Łukasz Gut <blinku.dev@gmail.com>
2020-04-18 21:56:26 +0200

    Minor changes

--- examples/lab05/ex02.py
+++ examples/lab05/ex02.py
@@ -1,17 +1,11 @@
-import random
 from graphly.algorithm import algorithm
 from graphly.generator import generator
 from graphly.plotter import plotter
 
-g = generator.generate("flow-network", 2)
+g = algorithm.ford_fulkerson(generator.generate("flow-network", 4), 0, 1)
+plotter.plot_flow_digraph(g, 'ex02g.png')
 
-# for e in g.get_edges():
-#     e.set_flow(random.randint(0, e.get_capacity()))
-
-gg = algorithm.ford_fulkerson(g, 0, 1)
-plotter.plot_flow_digraph(gg, 'ex02g.png')
-
-for e in gg.get_edges():
+for e in g.get_edges():
     print(f"edge: {e.get_tuple()}")
     print(f"flow: {e.get_flow()} ")
     print(f"capacity: {e.get_capacity()}\n")
