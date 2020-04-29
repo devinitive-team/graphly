617b60b446dfb968f9c6727a126b5608f83dad75
Łukasz Gut <blinku.dev@gmail.com>
2020-04-29 22:24:17 +0200

    Better png naming

--- examples/lab02/ex01.py
+++ examples/lab02/ex01.py
@@ -4,4 +4,4 @@ seq = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
 
 g = generator.generate("degree-seq", seq)
 g.print()
-g.plot()
+g.plot("ex01.png")
--- examples/lab02/ex02.py
+++ examples/lab02/ex02.py
@@ -5,6 +5,6 @@ seq = [2, 2, 2, 2, 2, 2, 2]
 
 if algorithm.is_degree_seq(seq):
     g = generator.generate("degree-seq", seq)
-    g.plot("g1.png")
+    g.plot("ex02_01.png")
     algorithm.randomize_graph(g, 3)
-    g.plot("g2.png")
+    g.plot("ex02_02.png")
